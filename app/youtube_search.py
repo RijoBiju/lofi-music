93d759f236044e0e53aeb8149f46ff20928c8c04
Rijo Biju <rijobijua.github@outlook.com>
2025-09-22 13:59:31 +0400

    youtube search

---
+++ app/youtube_search.py
@@ -0,0 +1,5 @@
+from youtube_search import YoutubeSearch
+
+def search_youtube(query, max_results=1):
+    results = YoutubeSearch(query, max_results=max_results).to_dict()
+    return results
--- main.py
+++ main.py
@@ -9,6 +9,9 @@ async def root():
 @app.get("/spotify-playlist")
 async def get_spotify_playlist_details(playlist_id: str = Query(...)):
     from app.spotify_client import get_playlist, get_track_from_playlist, get_artists_from_playlist, get_track_image
+    from app.youtube_search import search_youtube
     playlist = get_playlist(playlist_id)
     artist, track, image = get_artists_from_playlist(playlist[0]), get_track_from_playlist(playlist[0]), get_track_image(playlist[0])
-    return {'album_art': image, 'artist': artist, 'track': track}
+    search_result = search_youtube(query=f"{artist} {track}")
+    # return {'album_art': image, 'artist': artist, 'track': track}
+    return search_result
